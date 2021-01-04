683ea114090cb35bc4d850d97890cb569c1a46d8
Pablo Romero <pablo.romero@glovoapp.com>
2021-01-04 23:04:00 +0100

    Initial Commit

---
+++ Marvel-client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel-client
+//
+//  Created by Pablo Ezequiel Romero Giovannoni on 04/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Marvel-clientTests/Marvel_clientTests.swift
@@ -0,0 +1,33 @@
+//
+//  Marvel_clientTests.swift
+//  Marvel-clientTests
+//
+//  Created by Pablo Ezequiel Romero Giovannoni on 04/01/2021.
+//
+
+import XCTest
+@testable import Marvel_client
+
+class Marvel_clientTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
